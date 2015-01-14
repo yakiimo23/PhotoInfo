ee4ff99f3ef26260f0a7daacd0747e50c8978694
MasafumiMatsunaga <astromech12000@gmail.com>
2015-01-14 20:58:51 +0900

    Albumへのアクセス処理追加

--- PhotoInfo/ViewController.swift
+++ PhotoInfo/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+  @IBOutlet weak var mainImageView: UIImageView!
+  var picker:UIImagePickerController?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    picker = UIImagePickerController()
+    picker?.delegate = self
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +25,17 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+
+  @IBAction func openAlbum(sender: UIBarButtonItem) {
+    if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+      picker?.sourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum
+      self.presentViewController(picker!, animated: true, completion: nil)
+    }
+  }
+  @IBAction func launchCamera(sender: UIBarButtonItem) {
+  }
+  
+  
 
 }
 
